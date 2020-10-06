1aa18b8fa5626e41137c757be376454542ee02b9
sena986 <53561380+sena986@users.noreply.github.com>
2020-10-06 23:09:41 +0900

    painting part

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,16 +1,18 @@
 import { handleNewMsg } from "./chat";
 import { handelNewUser, handleDisconnected } from "./notifications";
+import { handleBeganPath, handleFilled, handleStrokedPath } from "./paint";
 
 let socket = null;
 
 export const getSocket = () => socket;
 
-export const updateSocket = (aSocket) => (socket = aSocket);
-
 export const initSockets = (aSocket) => {
   const { events } = window;
-  updateSocket(aSocket);
-  aSocket.on(events.newUser, handelNewUser);
-  aSocket.on(events.disconnected, handleDisconnected);
-  aSocket.on(events.newMsg, handleNewMsg);
+  socket = aSocket;
+  socket.on(events.newUser, handelNewUser);
+  socket.on(events.disconnected, handleDisconnected);
+  socket.on(events.newMsg, handleNewMsg);
+  socket.on(events.beganPath, handleBeganPath);
+  socket.on(events.strokedPath, handleStrokedPath);
+  socket.on(events.filled, handleFilled);
 };
